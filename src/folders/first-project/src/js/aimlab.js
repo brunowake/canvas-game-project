4c8fee6e76ba017ec39456480d75073333358ce0
Bruno Wake <brunowake.bw@gmail.com>
2022-02-19 11:20:13 -0300

    adding new class - game

--- src/folders/first-project/src/js/aimlab.js
+++ src/folders/first-project/src/js/aimlab.js
@@ -45,3 +45,131 @@ class Target {
     this.y = this.y + this.velocity.y;
   }
 }
+
+//game
+
+class Game {
+  constructor(player) {
+    this.targets = [];
+    this.maxOfTargets = 1;
+    this.velocityMultiplier = 1;
+    this.player = player;
+    this.click = {};
+    this.interval;
+  }
+
+  clearData() {
+    this.player.score = 0;
+    this.removeTargets(0, this.targets.length);
+  }
+
+  removeTargets(start, qtd) {
+    game.targets.splice(start, qtd);
+  }
+
+  updateScore(value) {
+    this.player.score += value;
+  }
+  //update position of each target on targets[]
+  updateTargetsPosition(target) {
+    target.updatePosition();
+  }
+  // getting angle
+  getAngle(objPosition) {
+    return Math.atan2(center.y - objPosition.y, center.x - objPosition.x);
+  }
+
+  // getting velocity
+  getVelocity(objPosition) {
+    return {
+      x: Math.cos(this.getAngle(objPosition)) * this.velocityMultiplier,
+      y: Math.sin(this.getAngle(objPosition)) * this.velocityMultiplier,
+    };
+  }
+
+  // getting random targets
+  getRandomTarget() {
+    const radius = randomNumber() * (40 - 20) + 20;
+    let x;
+    let y;
+    // getting random elements and rendering them outside the viewport
+    // if randomNumber < 0.5 the target will be rendered at the x axis otherwise it will be render at the y axis
+    randomNumber() < 0.5
+      ? //  if randomNumber < 0.5 the target will be rendered on the left side otherwise it will be rendered
+        //  on the right side. Both elements will be rendered at any high on the viewport
+        ((x = randomNumber() < 0.5 ? 0 - radius : canvas.width + radius),
+        (y = randomNumber() * canvas.height))
+      : ((x = randomNumber() * canvas.width),
+        (y = randomNumber() < 0.5 ? 0 - radius : canvas.height + radius));
+
+    const velocity = this.getVelocity({ x: x, y: y });
+
+    return {
+      x,
+      y,
+      radius,
+      color: "red",
+      velocity,
+    };
+  }
+
+  getDistance(target, element) {
+    // to know the distance you need to subtract the target(final position) from the element you want to compare
+    return Math.hypot(element.x - target.x, element.y - target.y);
+  }
+
+  // spawning enemies
+  spawnEnemies() {
+    const intervalId = setInterval(() => {
+      const target = new Target(this.getRandomTarget());
+      this.targets.length < this.maxOfTargets ? this.targets.push(target) : "";
+    }, 500);
+
+    this.interval = intervalId;
+  }
+
+  checkClick(updateElementCb) {
+    this.targets.forEach((target, index) => {
+      const distance = this.getDistance(target, this.click);
+      if (distance - target.radius * 1.4 <= 1) {
+        this.maxOfTargets++;
+        setTimeout((_) => {
+          this.removeTargets(index, 1);
+          this.velocityMultiplier += 0.02; //increment velocityMultiplier
+          this.updateScore(
+            Math.round((70 - target.radius) * this.velocityMultiplier)
+          );
+          updateElementCb(this.player.score);
+        }, 0);
+      }
+    });
+
+    this.click = {};
+  }
+
+  // checking if click matches target position
+
+  isGameOver(distance, target) {
+    return distance - target.radius - this.player.radius < 1 ? true : false;
+  }
+
+  // checking if target hits the player
+  playerColision(target) {
+    const distance = this.getDistance(this.player, target);
+    return this.isGameOver(distance, target);
+  }
+
+  gameOver(animationId) {
+    clearInterval(game.interval);
+    this.velocityMultiplier = 1;
+  }
+
+  checkColisionPerFrame(animationId, cb) {
+    this.targets.forEach((target) => {
+      this.updateTargetsPosition(target);
+      this.playerColision(target)
+        ? (this.gameOver(animationId), cb(animationId))
+        : "";
+    });
+  }
+}
--- src/folders/first-project/src/js/index.js
+++ src/folders/first-project/src/js/index.js
@@ -1,16 +1,10 @@
 const canvas = document.getElementById("myCanvas");
 const canvasContext = canvas.getContext("2d");
-const player = new Player(); // creating a player
 const scoreValueElement = document.getElementById("scoreValue");
 const startBtn = document.getElementById("startBtn");
 const modal = document.getElementById("modal");
 const finalScore = document.getElementById("finalScore");
-
-const targets = [];
-let maxOfTargets = 1;
-let velocityMultiplier = 1;
-let click = {};
-let interval;
+let game;
 
 // setting canvas to fullscreen
 canvas.width = window.innerWidth;
@@ -18,15 +12,12 @@ canvas.height = window.innerHeight;
 
 // game
 canvas.addEventListener("click", (event) => {
-  click = { x: event.clientX, y: event.clientY };
-  checkClick();
+  game.click = { x: event.clientX, y: event.clientY };
+  game.checkClick(updateScoreElement);
 });
 
 startBtn.addEventListener("click", (event) => {
-  modal.classList.add("hidden");
-  clearData();
-  interval = spawnEnemies();
-  animate();
+  startGame();
 });
 
 // update game score
@@ -38,106 +29,20 @@ function randomNumber() {
   return Math.random();
 }
 
-function removeTargets(start, qtd) {
-  targets.splice(start, qtd);
+function startGame() {
+  const player = new Player();
+  game = new Game(player);
+  modal.classList.add("hidden");
+  game.spawnEnemies();
+  game.clearData();
+  animate();
+  updateScoreElement(game.player.score);
 }
 
-function updateScore(value) {
-  player.score += value;
-}
-
-//update position of each target on targets[]
-function updateTargetsPosition(target) {
-  target.updatePosition();
-}
-
-// getting random targets
-function getRandomTarget() {
-  const radius = randomNumber() * (40 - 20) + 20;
-  let x;
-  let y;
-  // getting random elements and rendering them outside the viewport
-  // if randomNumber < 0.5 the target will be rendered at the x axis otherwise it will be render at the y axis
-  randomNumber() < 0.5
-    ? //  if randomNumber < 0.5 the target will be rendered on the left side otherwise it will be rendered
-      //  on the right side. Both elements will be rendered at any high on the viewport
-      ((x = randomNumber() < 0.5 ? 0 - radius : canvas.width + radius),
-      (y = randomNumber() * canvas.height))
-    : ((x = randomNumber() * canvas.width),
-      (y = randomNumber() < 0.5 ? 0 - radius : canvas.height + radius));
-
-  const velocity = getVelocity({ x: x, y: y });
-
-  return {
-    x,
-    y,
-    radius,
-    color: "red",
-    velocity,
-  };
-}
-
-// getting angle
-function getAngle(objPosition) {
-  return Math.atan2(center.y - objPosition.y, center.x - objPosition.x);
-}
-// getting velocity
-function getVelocity(objPosition) {
-  return {
-    x: Math.cos(getAngle(objPosition)) * velocityMultiplier,
-    y: Math.sin(getAngle(objPosition)) * velocityMultiplier,
-  };
-}
-
-// spawning enemies
-function spawnEnemies() {
-  const intervalId = setInterval(() => {
-    const target = new Target(getRandomTarget());
-    targets.length < maxOfTargets ? targets.push(target) : "";
-  }, 500);
-
-  return intervalId;
-}
-
-function getDistance(target, element) {
-  // to know the distance you need to subtract the target(final position) from the element you want to compare
-  return Math.hypot(element.x - target.x, element.y - target.y);
-}
-
-// checking if click matches target position
-function checkClick() {
-  targets.forEach((target, index) => {
-    const distance = getDistance(target, click);
-    if (distance - target.radius * 1.4 <= 1) {
-      maxOfTargets++;
-      setTimeout((_) => {
-        removeTargets(index, 1);
-        velocityMultiplier += 0.02; //increment velocityMultiplier
-        updateScore(Math.round((70 - target.radius) * velocityMultiplier));
-        updateScoreElement(player.score);
-      }, 0);
-    }
-  });
-
-  click = {};
-}
-
-function isGameOver(distance, target) {
-  return distance - target.radius - player.radius < 1 ? true : false;
-}
-
-function gameOver(animationId) {
+function stopGame(animationId) {
   window.cancelAnimationFrame(animationId);
-  finalScore.innerText = player.score.toFixed(0);
-  clearInterval(interval);
-  velocityMultiplier = 1;
+  finalScore.innerText = game.player.score.toFixed(0);
   modal.classList.remove("hidden");
-}
-
-// checking if target hits the player
-function playerColision(target, animationId) {
-  const distance = getDistance(player, target);
-  isGameOver(distance, target) ? gameOver(animationId) : "";
 }
 
 // animating canvas
@@ -146,16 +51,7 @@ function animate() {
   canvasContext.rect(0, 0, canvas.width, canvas.height);
   canvasContext.fillStyle = "#2F5373";
   canvasContext.fill();
-  player.draw();
+  game.player.draw();
 
-  targets.forEach((target) => {
-    updateTargetsPosition(target);
-    playerColision(target, animationId);
-  });
-}
-
-function clearData() {
-  player.score = 0;
-  removeTargets(0, targets.length);
-  updateScoreElement(player.score);
+  game.checkColisionPerFrame(animationId, stopGame);
 }
